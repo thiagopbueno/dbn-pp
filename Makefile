aac0c31f482c30f9fb5cd4ceb96e7d25b9598dbd
Thiago P Bueno <thiago.pbueno@gmail.com>
2015-12-29 10:14:14 -0200

    Read from stdin and print to stdout uai model

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC=g++
+CCFLAGS=-Wall -ansi -pedantic -std=c++11
+
+OBJ=bin/variable.o bin/factor.o bin/domain.o bin/io.o bin/main.o
+
+all: dbn
+
+dbn: $(OBJ)
+	$(CC) -o $@ $^
+
+bin/%.o: src/%.cpp
+	$(CC) $(CCFLAGS) -I include/ -O2 -c -o $@ $<
+
+.PHONY: clean
+
+clean:
+	rm -rfv dbn bin/*.o
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+#include "io.h"
+#include "variable.h"
+#include "factor.h"
+
+using namespace std;
+
+void print_model(vector<msp::Variable> variables, vector<msp::Factor> factors)
+{
+	cout << "Model: " << variables.size() << " variables, " << factors.size() << " factors. " << endl;
+	for (auto v: variables) { cout << v << endl; }
+	for (auto f: factors)   { cout << f << endl; }
+	cout << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	vector<msp::Variable> variables;
+    vector<msp::Factor> factors;
+
+    msp::load_uai_model(variables, factors);
+    print_model(variables, factors);
+
+	return 0;
+}
