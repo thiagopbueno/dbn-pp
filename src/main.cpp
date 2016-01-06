4968dee71056ad05f40753ed64db415b4208b111
Thiago P Bueno <thiago.pbueno@gmail.com>
2016-01-06 16:24:45 -0200

    Print model components in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,12 @@
 using namespace std;
 using namespace dbn;
 
+void print_model(
+    char *filename,
+    vector<unique_ptr<Variable>> &variables, vector<shared_ptr<Factor>> &factors,
+    vector<unsigned> &prior, unordered_map<unsigned,const Variable*> &transition, vector<unsigned> &sensor
+);
+
 int main(int argc, char *argv[])
 {
     unsigned order;
@@ -38,12 +44,13 @@ int main(int argc, char *argv[])
     unordered_map<unsigned,const Variable*> transition;
     vector<unsigned> sensor;
 
-    vector<unordered_map<unsigned,unsigned>> observations;
-
     if (read_uai_model(argv[1], order, variables, factors, prior, transition, sensor)) return -1;
+    print_model(argv[1], variables, factors, prior, transition, sensor);
+
+    vector<unordered_map<unsigned,unsigned>> observations;
     if (read_observations(argv[2], observations)) return -2;
 
-    cout << ">> FILTERING" << endl;
+    cout << ">> FILTERING:" << endl;
     vector<shared_ptr<Factor>> estimates = filtering(factors, prior, transition, sensor, observations);
 
     unsigned t = 1;
@@ -61,4 +68,47 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+void print_model(
+    char *filename,
+    vector<unique_ptr<Variable>> &variables, vector<shared_ptr<Factor>> &factors,
+    vector<unsigned> &prior, unordered_map<unsigned,const Variable*> &transition, vector<unsigned> &sensor) {
+
+    cout << ">> MODEL: " << filename << endl << endl;
+
+    cout << "=== Variables ===" << endl;
+    for (auto const& pv : variables) {
+        cout << *pv << endl;
+    }
+    cout << endl;
+
+    cout << "=== Factors ===" << endl;
+    for (auto const& pf : factors) {
+        cout << *pf << endl;
+    }
+    cout << endl;
+
+    cout << "=== Prior model ===" << endl;
+    cout << "Variables {";
+    for (auto id : prior) {
+        cout << " " << id;
+    }
+    cout << " }" << endl << endl;
+
+    cout << "=== Transition model ===" << endl;
+    cout << "Variables {";
+    for (auto it : transition) {
+        unsigned id_next = it.first;
+        unsigned id_curr = it.second->id();
+        cout << " " << id_next << "->" << id_curr;
+    }
+    cout << " }" << endl << endl;
+
+    cout << "=== Sensor model ===" << endl;
+    cout << "Variables {";
+    for (auto id : sensor) {
+        cout << " " << id;
+    }
+    cout << " }" << endl << endl;
 }
