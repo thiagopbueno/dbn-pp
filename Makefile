e2a1fd9d59f83b7cdb44ce95bd323c32d04bb530
Thiago P Bueno <thiago.pbueno@gmail.com>
2016-01-12 18:51:37 -0200

    Add CUDD include and library file references to Makefile

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ CCFLAGS=-Wall -Wextra -ansi -pedantic -std=c++11
 OBJ=bin/variable.o bin/domain.o bin/factor.o bin/io.o bin/operations.o bin/inference.o bin/main.o
 OBJDEBUG=debug/variable.o debug/domain.o debug/factor.o debug/io.o debug/operations.o debug/inference.o debug/main.o
 
-INCLUDE=-Iinclude
+CUDD=/usr/local/CUDD/cudd-3.0.0
+
+INCLUDE=-Iinclude -I$(CUDD)/cudd -I$(CUDD)/cplusplus
+
+LIBS=$(CUDD)/cudd/.libs/libcudd.a
 
 all: dbn
 
@@ -12,7 +16,7 @@ install: clean
 	mkdir bin/ debug/
 
 dbn: $(OBJ)
-	$(CC) -o $@ $^
+	$(CC) -o $@ $^ $(LIBS)
 
 bin/%.o: src/%.cpp include/%.h
 	$(CC) $(CCFLAGS) $(INCLUDE) -O2 -c -o $@ $<
@@ -34,4 +38,4 @@ debug/main.o: src/main.cpp
 
 .PHONY: clean
 clean:
-	rm -rfv dbn bin/*.o dbn-debug dbn-debug.dSYM/ debug/*.o
+	rm -rfv dbn bin/*.o dbn-debug dbn-debug.dSYM/ dbn.dSYM/ debug/*.o
--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,16 @@
 #include "operations.h"
 #include "inference.h"
 
+#include "stdio.h"
 #include <iomanip>
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <set>
 #include <memory>
+
+#include "cuddObj.hh"
+// #include "cudd.h"
 
 using namespace std;
 using namespace dbn;
@@ -39,6 +43,25 @@ void print_observations(vector<unordered_map<unsigned,unsigned>> &observations);
 
 void print_trajectory(vector<shared_ptr<Factor>> &states, set<unsigned> &state_variables);
 
+void print_test_add()
+{
+    Cudd mgr(2,0);
+    ADD p = mgr.addVar(0);
+    ADD q = mgr.addVar(1);
+
+    // Test arithmetic operators.
+    ADD r = p + q;
+    cout << "r"; r.print(2,2);
+
+    FILE *f = fopen("add_test.dot", "w");
+    const char *inputNames[2]{"p", "q"};
+    const char *outputNames[1]{"r"};
+    DdNode *outputs[1];
+    outputs[0] = r.getNode();
+    Cudd_DumpDot(mgr.getManager(), 1, outputs, inputNames, outputNames, f);
+    fclose(f);
+}
+
 int main(int argc, char *argv[])
 {
     unsigned order;
@@ -48,6 +71,8 @@ int main(int argc, char *argv[])
     vector<unsigned> prior;
     unordered_map<unsigned,const Variable*> transition;
     vector<unsigned> sensor;
+
+    print_test_add();
 
     if (read_uai_model(argv[1], order, variables, factors, prior, transition, sensor)) return -1;
     cout << ">> MODEL: " << argv[1] << endl;
